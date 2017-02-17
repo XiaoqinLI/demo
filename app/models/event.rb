7b8cda5117a5fa7ac00af98026fa978369b807d6
Vincent LI <daybreaklee@utexas.edu>
2017-02-16 22:37:41 -0600

    add a few more model and relationship

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ActiveRecord::Base
 	validates_presence_of :name
-	has_many :attendees # plural
+	has_many :attendees, ->{ order("id DESC") }, :dependent => :destroy # plural
+	belongs_to :category
+  has_one :location, :dependent => :destroy
+  has_many :event_groupships
+  has_many :groups, :through => :event_groupships
 end
